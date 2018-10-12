5c90182c12aca162dbdcf1e6e9b4ee4590a9987b
Eitan Alkaslassy <eitanalkaslassy@Eitans-MacBook-Pro.local>
2018-10-12 08:44:11 -0400

    login page and firebase auth

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
+import LoginPage from '../components/LoginPage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
@@ -12,8 +13,9 @@ const AppRouter = () => (
     <div>
       <Header />
       <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+        <Route path="/" component={LoginPage} exact={true}/>
         <Route path="/create" component={AddExpensePage} />
+        <Route path="/dashboard" component={ExpenseDashboardPage} />
         <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
